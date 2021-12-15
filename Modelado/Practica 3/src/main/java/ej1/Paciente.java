305514714466bb36f02ed8d70b4034860cc358b4
jxtaaa <jlbp@uma.es>
2021-12-15 14:23:08 +0100

    Comenzada P3

---
+++ Modelado/Practica 3/src/main/java/ej1/Paciente.java
@@ -0,0 +1,27 @@
+package ej1;
+
+import java.util.List;
+
+public class Paciente {
+    private Expediente expedienteAbierto;
+    private List<Expediente> expedientes;
+
+    Paciente(){
+        //TODO
+    }
+
+    public static Paciente crearPaciente(){
+        //TODO
+        return null;
+    }
+
+    public static Expediente crearExpediente(){
+        //TODO
+        return null;
+    }
+
+    public List<Expediente> getExpedientes(){
+        //TODO
+        return null;
+    }
+}
