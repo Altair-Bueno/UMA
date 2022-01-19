8d165ce62b1b97563db01c586d37572d677b0762
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-01-19 17:31:52 +0100

    new(ej1): instanciate class using get method

--- Modelado/Practica 4/src/main/java/Ej1/x/X.java
+++ Modelado/Practica 4/src/main/java/Ej1/x/X.java
@@ -1,6 +1,12 @@
 package Ej1.x;
 
+import Ej1.Client;
+
 public abstract class X {
+  public static X getInstance(Client caller) {
+    return new Proxy(caller);
+  }
+
   public abstract void rutina1();
 
   public abstract void rutina2(double y);
