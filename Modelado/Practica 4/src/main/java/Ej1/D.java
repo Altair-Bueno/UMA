52b2b12a4298e0ea7698ea776d1d012ffdacd980
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-01-25 14:03:00 +0100

    fix(p4/ej1): d uses null

--- Modelado/Practica 4/src/main/java/Ej1/D.java
+++ Modelado/Practica 4/src/main/java/Ej1/D.java
@@ -3,13 +3,9 @@ package Ej1;
 import Ej1.x.XProxy;
 
 public class D extends XProxy {
-  /**
-   * Construye un nuevo proxy que guarda una instancia de XService
-   *
-   * @param client clase que llama los métodos
-   */
-  public D(Client client) {
-    super(client);
+  /** Construye un nuevo proxy que guarda una instancia de XService */
+  public D() {
+    super(null);
   }
 
   @Override
