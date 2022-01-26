68d6ce4afb29d530e8b30b3f36b2d953378834b3
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-01-26 18:08:14 +0100

    fix(p4/ej2a): javadoc

--- Modelado/Practica 4/src/main/java/Ej2a/Biestable.java
+++ Modelado/Practica 4/src/main/java/Ej2a/Biestable.java
@@ -1,22 +1,48 @@
 package Ej2a;
 
+/**
+ * Semáforo Biestable
+ *
+ * <p>El semáforo Biestable transita entre dos estados posibles utilizando los métodos {@link
+ * Biestable#abrir()} y {@link Biestable#cerrar()}
+ */
 public class Biestable {
 
     private EstadoBiestable estado;
 
-    public Biestable(){
+  /** Crea un nuevo semáforo Biestable en el estado inicial Rojo */
+  public Biestable() {
         this.estado=new Rojo();
     }
 
-    public String estado(){
+  /** Devuelve una cadena indicando el estado actual */
+  public String estado() {
         return estado.estado();
     }
 
-    public void abrir(){
+  /**
+   * Abre el semáforo
+   *
+   * <p>Posibles transiciones:
+   *
+   * <p>{@code Rojo -> Verde}
+   *
+   * @throws IllegalCallerException Si la máquina de estados no permite esa transición
+   */
+  public void abrir() {
         estado.abrir(this);
     }
 
-    public void cerrar(){
+  /**
+   * Cierra el semáforo
+   *
+   * <p>Posibles transiciones:
+   *
+   * <p>{@code Rojo -> Verde}
+   *
+   * @throws IllegalCallerException Si la máquina de estados no permite esa transición
+   */
+  public void cerrar() {
         estado.cerrar(this);
     }
 
