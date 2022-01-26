344473d0137a0a6b50db0e45ca469e15575100c7
jxtaaa <74137433+jxtaaa@users.noreply.github.com>
2022-01-26 18:21:17 +0100

    Ej2c done

--- Modelado/Practica 4/src/main/java/Ej2c/MediadorBiestableTriestable.java
+++ Modelado/Practica 4/src/main/java/Ej2c/MediadorBiestableTriestable.java
@@ -19,14 +19,14 @@ public class MediadorBiestableTriestable implements Mediador{
     }
 
     public void cambiarABiestable(){
-        if(semaforoActual.estado instanceof Amarillo){
+        if(semaforoActual.getEstado() instanceof Amarillo){
             throw new IllegalStateException();
         }
-        semaforoActual = new Biestable(semaforoActual.estado);
+        semaforoActual = new Biestable(semaforoActual.getEstado());
     }
 
     public void cambiarATriestable(){
-        semaforoActual = new Triestable(semaforoActual.estado);
+        semaforoActual = new Triestable(semaforoActual.getEstado());
     }
 
     public void abrir(){
