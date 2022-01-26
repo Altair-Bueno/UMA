c516b5bfdf33226b11d585952016e482af2231f3
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-01-26 17:09:05 +0100

    doc(p4/ej3): added javadoc

--- Modelado/Practica 4/src/main/java/Ej3/DateSortStrategy.java
+++ Modelado/Practica 4/src/main/java/Ej3/DateSortStrategy.java
@@ -1,5 +1,6 @@
 package Ej3;
 
+/** Ordena los emails por fecha */
 public class DateSortStrategy implements SortStrategy {
   @Override
   public boolean before(Email e1, Email e2) {
--- Modelado/Practica 4/src/main/java/Ej3/SortStrategy.java
+++ Modelado/Practica 4/src/main/java/Ej3/SortStrategy.java
@@ -1,5 +1,12 @@
 package Ej3;
 
 public interface SortStrategy {
+  /**
+   * Compara dos emails
+   *
+   * @param e1 primer email
+   * @param e2 segundo email
+   * @return true si el primer email está antes que el segundo email
+   */
   boolean before(Email e1, Email e2);
 }
